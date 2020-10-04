041b30eebeaafbf6318d77677e6edb866b52ee81
pablog <pablo4761@gmail.com>
2020-10-04 18:02:39 -0500

    primer

---
+++ php/conexion_be.php
@@ -0,0 +1,17 @@
+<?php
+
+
+    $conexion = mysqli_connect("localhost", "root", "" ,"login_register_db");
+
+    /*conprobarconexion
+    if($conexion){
+        echo 'conectado exitosamente a la base de Datos';
+    }else{
+        echo 'No se ha podido conectar a la base de Datos';
+    }
+    */
+
+
+
+
+?> 
