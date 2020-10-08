fcdcf4fe491243e476398828da2679f3f6467326
juanPabloG <68938644+juanPabloG@users.noreply.github.com>
2020-10-08 17:19:19 -0500

    Update and rename conexion_be.php to con_db.php

---
+++ php/con_db.php
@@ -0,0 +1,9 @@
+<?php
+// $conex = mysqli_connect("Servidor / host","Nombre de usuario","Passwd del usuario","Nombre de la BD")
+$conex = mysqli_connect("db4free.net","bike4life","passbike4life","dbbike4life");
+
+if ($conex) {
+    echo "La conexion a  sido todo un exito <br>";
+}
+
+?>
